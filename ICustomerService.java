9e113fe49638ae54e62ad81a078756397e656d61
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2022-07-06 21:54:50 +0530

    Add files via upload

---
+++ ICustomerService.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+import com.anshumanbiswal.crm.entity.Customer;
+
+public interface ICustomerService {
+
+	 List<Customer> getCustomers();
+
+	 void saveCustomer(Customer theCustomer);
+
+	 Customer getCustomer(int theId);
+
+	 void deleteCustomer(int theId);
+	
+}
